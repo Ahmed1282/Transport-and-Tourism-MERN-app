e9e740838293332461f757e58143dc8aa7f21ed3
Fatima Farooq <i202304@nu.edu.pk>
2023-12-04 20:57:00 +0500

    Add Dashboard component to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ManageRoutesPage from "./components/pages/admin/ManageRoutesPage";
+import Dashboard from "./components/pages/admin/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,7 +17,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </Routes> */}
 
         <Routes>
-          <Route path="/manage-routes" element={<ManageRoutesPage />}>
+          <Route path="/" element={<ManageRoutesPage />}>
+            {/* <Route index element={<ManageR />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} /> */}
+          </Route>
+
+          <Route path="/dashboard" element={<Dashboard />}>
             {/* <Route index element={<ManageR />} />
             <Route path="blogs" element={<Blogs />} />
             <Route path="contact" element={<Contact />} />
